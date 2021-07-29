80398b31293618f7297ff18b3d0ceccda86084a6
unknown <shaharil85@gmail.com>
2021-07-29 08:44:16 +0800

    make it better

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Socket } from 'ngx-socket-io';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  message = '';
+  messages = [];
+  currentUser = '';
+
+  constructor(private socket:Socket,  private toastCtrl: ToastController) {}
+  ngOnInit(){
+    this.socket.connect();
+    let name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+    //this.currentUser = 'shaharil';
+    this.socket.emit('set-name', name);
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+      }
+    });
+ 
+    this.socket.fromEvent('message').subscribe(message => {
+      this.messages.push(message);
+    });
+    
+  }
+
+  sendMessage() {
+    this.socket.emit('send-message', { text: this.message });
+    this.message = '';
+  }
+ 
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+ 
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+  
+
+
+}
